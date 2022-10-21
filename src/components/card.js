58a3ad8566b299fcb1ad5a9f4562cb8e08f26e0b
Steeven1995 <gabinmoundziegou@gmail.com>
2022-10-21 14:04:18 +0200

    correction de faute

--- src/components/card.js
+++ src/components/card.js
@@ -12,7 +12,7 @@ function card({student}) {
                         <a className="btn btn-sm-square btn-primary mx-1" target="_blank" href={student.linkedin}><i className="fab fa-linkedin-in"></i></a>
                         <a className="btn btn-sm-square btn-primary mx-1" href="#"><i className="fab fa-twitter"></i></a>
                         <a className="btn btn-sm-square btn-primary mx-1" target="_blank" href={student.facebook}><i className="fab fa-facebook-f"></i></a>
-                        <a className="btn btn-sm-square btn-primary mx-1" target="_blank" href={"http://"+student.site}><i className="fa fa-globe"></i></a>
+                        <a className="btn btn-sm-square btn-primary mx-1" target="_blank" href={"https://"+student.site+""}><i className="fa fa-globe"></i></a>
                     </div>
                 </div>
                 <div className="text-center p-4">
