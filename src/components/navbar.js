353777eccb5067212874a4ae39cdbfa125ff4695
Jordane YENO ADEMBIGONDJO <adembigondjo@gmail.com>
2022-10-24 12:18:13 +0100

    'newAdd'

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
-        <Link to='' className="navbar-brand d-flex align-items-center px-4 px-lg-5">
+        <a target="_blank" href='https://ecole241.org/' className="navbar-brand d-flex align-items-center px-4 px-lg-5">
             <img src="./img/logo.png" alt="" className="logo"/>        
-        </Link>
+        </a>
         <button type="button" style={{backgroundColor:"#ff3333"}} className="navbar-toggler btn btn-primary py-4 px-md-2" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             Nous contactez
         </button>
@@ -15,7 +15,7 @@ function navbar() {
                 <Link to="port-gentil" className="nav-item nav-link">Port-Gentil</Link>
                 <Link to="libreville" className="nav-item nav-link">Libreville</Link>
             </div>
-            <a href="" className="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Nous contactez<i className="fa fa-arrow-right ms-3"></i></a>
+            <a target="_blank" href="https://ecole241.org/contact/" className="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Nous contactez<i className="fa fa-arrow-right ms-3"></i></a>
         </div>
     </nav>
   )
