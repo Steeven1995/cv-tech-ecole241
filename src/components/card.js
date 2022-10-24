15cd0cd232f24c2ce62cad32bb6997a18ad324ec
Jordane YENO ADEMBIGONDJO <adembigondjo@gmail.com>
2022-10-24 12:22:01 +0100

    'newAdd'

--- src/components/card.js
+++ src/components/card.js
@@ -22,7 +22,7 @@ function card({student}) {
             <div className="position-relative d-flex justify-content-center" style={{marginTop: "-23px"}}>
                 <div className="bg-light d-flex justify-content-center pt-2 px-1">
                     <a className="btn btn-sm-square btn-primary mx-1" target="_blank" href={student.linkedin}><i className="fab fa-linkedin-in"></i></a>
-                    <a className="btn btn-sm-square btn-primary mx-1" href={student.cv[0].url}><i className="fa fa-file"></i></a>
+                    <a className="btn btn-sm-square btn-primary mx-1" target="_blank" href={student.cv[0].url}><i className="fa fa-file"></i></a>
                 </div>
             </div>
             <div className="text-center p-4">
