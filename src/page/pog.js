757f9b19f4cd66030717d5893d31e59d1197a3c3
Jordane YENO <104835883+JordaneYeno@users.noreply.github.com>
2022-10-25 19:11:50 +0100

    Update pog.js

--- src/page/pog.js
+++ src/page/pog.js
@@ -8,8 +8,8 @@ function Pog({students}) {
         <div className="container-xxl py-5">
             <div className="container">
                 <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 className="section-title bg-white text-center text-primary px-3">Référents digitals</h6>
                     <h1 className="mb-5">Nos apprenants</h1>
+                    <h6 className="section-title bg-white text-center text-primary px-3">Référents digitals</h6>
                 </div>
                 <div className="row g-4">
                     {students.map((student, index)=>{
@@ -22,8 +22,8 @@ function Pog({students}) {
         <div className="container-xxl py-5">
             <div className="container">
                 <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 className="section-title bg-white text-center text-primary px-3">Développeur web & Web mobile</h6>
                     <h1 className="mb-5">Nos apprenants</h1>
+                    <h6 className="section-title bg-white text-center text-primary px-3">Développeur web & Web mobile</h6>
                 </div>
                 <div className="row g-4">
                     {students.map((student, index)=>{
