b1e3255cfa5d1a4a12d99656c73dab255ddc6481
Jordane YENO <104835883+JordaneYeno@users.noreply.github.com>
2022-10-25 19:14:46 +0100

    Update libreville.js

--- src/page/libreville.js
+++ src/page/libreville.js
@@ -9,8 +9,8 @@ function Libreville({students}) {
         <div className="container-xxl py-5">
             <div className="container">
                 <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 className="section-title bg-white text-center text-primary px-3">Référents digitals</h6>
                     <h1 className="mb-5">Nos apprenants</h1>
+                    <h6 className="mb-3 section-title bg-white text-center text-primary px-3">Référents digitals</h6>
                 </div>
                 <div className="row g-4">
                     {students.map((student, index)=>{
@@ -23,8 +23,8 @@ function Libreville({students}) {
         <div className="container-xxl py-5">
             <div className="container">
                 <div className="text-center wow fadeInUp" data-wow-delay="0.1s">
-                    <h6 className="section-title bg-white text-center text-primary px-3">Développeur web & Web mobile</h6>
                     <h1 className="mb-5">Nos apprenants</h1>
+                    <h6 className="mb-3 section-title bg-white text-center text-primary px-3">Développeur web & Web mobile</h6>
                 </div>
                 <div className="row g-4">
                     {students.map((student, index)=>{
